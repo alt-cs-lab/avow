eace17001b786668cd8d488bdfb7e23d3e1c2f64
Nathan H. Bean <nhbean@ksu.edu>
2024-04-17 16:23:05 -0500

    Create fail.js

---
+++ src/fail.js
@@ -0,0 +1,20 @@
+const formatString = require('./format-string');
+const formatList = require("./format-list");
+
+/** @function fail
+ * Prints a fail statement and any feedback
+ * @param {string[]|string} feedback - the feedback to report 
+ * @returns {string} the HTML snippet
+*/
+function fail(feedback) {
+    if(Array.isArray(feedback)) {
+        process.stdout.write(`You need to fix the following issues:\n`);
+        process.stdout.write(`<ul>\n${formatList(feedback)}\n</ul>`);
+    } else if(feedback) {
+        process.stdout.write(`You need to fix the following issue:`);
+        process.stdout.write(formatString(feedback));
+    } 
+    process.exit(1);
+}
+
+module.exports = fail;
